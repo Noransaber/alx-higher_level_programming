bd6d0347c08e577714138c66b83bb9c6010cf133
Noransaber <noransaber685@gmail.com>
2023-06-06 06:24:00 -0700

    delet by mistake

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for ch in range(97, 123):
-    print("{}".format(chr(ch)), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for ch in range(97, 123):
-    if ch == 113 or ch == 101:
-        continue
-    else:
-        print("{}".format(chr(ch)), end='')
