46bd396f7b1a673c0483ab6f72cb1ca921d9c603
Noransaber <noransaber685@gmail.com>
2023-06-09 04:49:06 -0700

    edit code for task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """retrieves an element from a list like in C."""
+
+
 def element_at(my_list, idx):
-    length = len(my_list)
-    if idx < 0:
-        return None
-    elif idx > length:
-        return None
+    if idx >= 0 and idx < len(my_list):
+        return my_list[idx]
     else:
-        print("Element at index {} is {}".format(idx, ))
+        return None
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Write a function that adds 2 tuples."""
+def add_tuple(tuple_a=(), tuple_b=()):
+
