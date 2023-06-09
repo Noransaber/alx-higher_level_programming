7e54fab84bd9a7341c8f85de6508474930cf210c
Noransaber <noransaber685@gmail.com>
2023-06-09 05:53:57 -0700

    source code of task 12

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 """Write a function that adds 2 tuples."""
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_a[:2] + (0, 0)
+    tuple_b = tuple_b[:2] + (0, 0)
+
+    first_ele = tuple_a[0] + tuple_b[0]
+    sec_ele = tuple_b[1] + tuple_b[1]
+    return (first_ele, sec_ele)
+
 
