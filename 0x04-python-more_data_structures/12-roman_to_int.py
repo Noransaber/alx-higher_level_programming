617cc535e675dd39196f60b675bf68f6e4659202
Noransaber <noransaber685@gmail.com>
2023-06-14 10:33:24 -0700

    Last task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
+def sub(num_list):
+    to_sub = 0
+    max_list = max(num_list)
 
-def sub(list_num):
-    return max(list_num)  - sum(list_num)
+    for i in num_list:
+        if max_list > i:
+            to_sub += i
+
+    return (max_list - to_sub)
 
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not roman_string:
         return 0
-    
-    roman = {"I": 1, "V": 5, "L": 50, "C": 100, "D": 500, "M":1000}
-    list_key = list(roman.keys())
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(roman.keys())
+
     number = 0
-    last = 0
-    list_num = []
+    last_roman = 0
+    list_number = [0]
 
     for c in roman_string:
-        for c in roman:
-            current_val = roman[c]
-            if current_val <= last:
-                number += sub(list_num)
-                list_num = [current_val]
-            else:
-                list_num.append(current_val)
+        for r in list_keys:
+            if r == c:
+                if roman.get(c) <= last_roman:
+                    number += sub(list_number)
+                    list_number = [roman.get(c)]
+                else:
+                    list_number.append(roman.get(c))
 
-            last = current_val
+                last_roman = roman.get(c)
 
-        number += sub(list_num)
-        return number
+    number += sub(list_number)
+
+    return (number)
