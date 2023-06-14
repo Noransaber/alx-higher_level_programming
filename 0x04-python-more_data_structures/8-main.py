c6c453308b5a56f8e1bd9b2d5136c9c58950c45f
Noransaber <noransaber685@gmail.com>
2023-06-14 07:29:55 -0700

    Code for delte

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -3,7 +3,7 @@ simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+a_dictionary = {'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3]}
 new_dict = simple_delete(a_dictionary, 'track')
 print_sorted_dictionary(a_dictionary)
 print("--")
