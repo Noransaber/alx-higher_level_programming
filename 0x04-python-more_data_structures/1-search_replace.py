40fb7471dd1e67633bdde57ea2503e3a3396ee0a
Noransaber <noransaber685@gmail.com>
2023-06-14 04:40:40 -0700

    Remove triling space

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """replaces all occurrences of an 
+    """replaces all occurrences of an
     element by another in a new list."""
 
     new_list = [replace if item == search else item for item in my_list]
