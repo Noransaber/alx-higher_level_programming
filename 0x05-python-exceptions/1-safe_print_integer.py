a3a5a53897f5e7bdaf51888befa8e6624cc23639
Noransaber <noransaber685@gmail.com>
2023-06-26 04:59:38 -0700

    Updating

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
             print("{:d}".format(value))
             return True
     except (TypeError, ValueError):
