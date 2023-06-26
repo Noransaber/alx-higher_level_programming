b7e91ef9d8a990ab81148a4febbbe609f5f76869
Noransaber <noransaber685@gmail.com>
2023-06-26 04:39:27 -0700

    Updating

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        counter = 0
-        for i in my_list:
-            if counter <= x:
-                print("{}".format(i), end="")
-                counter += 1
-            else:
-                break
-    except IndexError:
-        pass
-    finally:
-        print("")
-        return counter
+    counter = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            counter += 1
+        except IndexError:
+            break
+    print("")
+    return counter
