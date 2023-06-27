d30f84a6b834bb030b1ea5b1b8ab2c8bf19777fa
Noransaber <noransaber685@gmail.com>
2023-06-27 12:42:08 -0700

    Code

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Define a class"""
+
+
+class Square:
+    """ Represent a square"""
+
+    def __init__(self, size):
+        """Initialize a new square"""
+
+        self.__size = size
