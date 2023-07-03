1351acf26e6eed35c1bba3d0413c20eab76ac774
Noransaber <noransaber685@gmail.com>
2023-07-03 04:43:15 -0700

    Remove a line for style

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,7 +29,6 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-
     @property
     def height(self):
         """Get the value of the height"""
