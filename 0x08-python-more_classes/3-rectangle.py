638b9a25122f31e3fb77919df85a0978bdab020d
Noransaber <noransaber685@gmail.com>
2023-07-03 06:02:29 -0700

    Code

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Class retangel"""
+
+
+class Rectangle:
+    """Define a class"""
+
+    def __init__(self, width=0, height=0):
+        """Init a new retangle
+        Args:
+        ===
+        width: int
+        height: init
+
+        Method:
+        Area: Calculate are
+        Perimeter: Calaculate it
+
+        Raise:
+        TypeError: if not int
+        ValueError: if negative
+
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Get the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError(" width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return("")
+
+        r = []
+        for i in range(self.__height):
+            [r.append('#') for k in range(self.__width)]
+            if i != self.__height - 1:
+                r.append("\n")
+        return ("".join(r))
