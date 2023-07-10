952ce78a4ed84b1e375bfa7f7c114ed5c19b5555
Noransaber <noransaber685@gmail.com>
2023-07-10 02:20:23 -0700

    Source code

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define an aboj attribute lookup function"""
+def lookup(obj):
+    """
+     function that returns the list of 
+     available attributes and 
+     methods of an object:
+     """
+     return dir(obj)
