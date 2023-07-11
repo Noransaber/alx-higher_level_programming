f0d9296c50bea626e07b7555365549b368e6911d
Noransaber <noransaber685@gmail.com>
2023-07-11 19:59:49 +0300

    Updates

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,20 +1,36 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
 class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Init a new Student.
+
+        Args:
+            first_name (str):
+            last_name str
+            age (int)
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs is None:
-            attrs = []
-        elif not isinstance(attrs, list):
-            raise TypeError("attrs must be a list")
-        elif not all(isinstance(attr, str) for attr in attrs):
-            raise TypeError("attrs must contain only strings")
-        return {attr: getattr(self, attr) for attr in attrs}
+        """Get a dic representation
+        Args:
+            attrs (list).
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        if not isinstance(json, dict):
-            raise TypeError("json must be a dictionary")
+        """Replace the attrs.
+        Args:
+            json dict
+        """
         for k, v in json.items():
             setattr(self, k, v)
