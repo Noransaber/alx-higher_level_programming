631fa8a786644ae4b2895fb1be2c08303aae224b
Noransaber <noransaber685@gmail.com>
2023-07-11 19:43:50 +0300

    Update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,14 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def main():
-  list_of_items = []
-  for item in sys.argv[1:]:
-    list_of_items.append(item)
 
-  save_to_json_file(list_of_items, "add_item.json")
+def main():
+    list_of_items = []
+    for item in sys.argv[1:]:
+        list_of_items.append(item)
+
+    save_to_json_file(list_of_items, "add_item.json")
+
 
 if __name__ == "__main__":
-  main()
+    main()
