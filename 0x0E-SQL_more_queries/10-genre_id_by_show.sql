e782ed0667ec0a31775752266ffdeaa2f81c88e8
Noransaber <noransaber685@gmail.com>
2023-08-16 20:21:30 +0300

    TASKS from 10 to 16

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 --Import the database dump from hbtn_0d_tvshows to your MySQL server: download
-
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id ASC;
---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql~
@@ -0,0 +1,2 @@
+--Import the database dump from hbtn_0d_tvshows to your MySQL server: download
+
