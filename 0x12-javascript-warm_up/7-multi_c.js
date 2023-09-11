5e2b5f3efb077f24ad8277022b2833e3751c20b6
Noransaber <noransaber685@gmail.com>
2023-09-11 13:57:55 +0300

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+num = parseInt(process.argv[2]);
+let i;
+
+if (!num) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
