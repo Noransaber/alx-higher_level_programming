527bb46a0dff5d18caf1f777d7f6b088b4d9e153
Noransaber <noransaber685@gmail.com>
2023-09-12 15:51:34 +0300

    Source code for first 5 tasks

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  print () {
+    let i;
+    for (i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
