1db59dc7c13f7f9f0d39d848564a2f5556fb214e
Noransaber <noransaber685@gmail.com>
2023-09-14 17:03:50 +0300

    updates

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Listes all states"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    MyDb = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf8')
+
+    myCur = MyDb.cursor()
+
+    myCur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    rows = myCur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    myCur.close()
+    MyDb.close()
+
+
---
+++ 0x12-javascript-warm_up/13-add.js~
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+export.add = function (a, b) {
+  return a + b;
+};
