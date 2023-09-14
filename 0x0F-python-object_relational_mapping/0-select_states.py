de089c20b06b6405020613bc329842d00e125671
Noransaber <noransaber685@gmail.com>
2023-09-14 19:20:38 +0300

    Task 2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,15 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ == '__main__':
-    MyDb = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf8')
+if __name__ == "__main__":
+    MyDb = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8",
+    )
 
     myCur = MyDb.cursor()
 
@@ -19,5 +26,3 @@ if __name__ == '__main__':
 
     myCur.close()
     MyDb.close()
-
-
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,8 +5,14 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8",
+    )
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
