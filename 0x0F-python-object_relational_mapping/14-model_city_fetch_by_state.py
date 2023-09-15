a2fa2248d73041362dca154de37b1ba442b7746f
Noransaber <noransaber685@gmail.com>
2023-09-15 23:38:40 +0300

    Updates

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,6 +4,7 @@ from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_city import City
 
 if __name__ == "__main__":
     engine = create_engine(
