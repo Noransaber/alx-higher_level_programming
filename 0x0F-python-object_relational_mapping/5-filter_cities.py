11974510c221eb50b573303ada9b911f953519f1
Noransaber <noransaber685@gmail.com>
2023-09-15 20:35:32 +0300

    Updates

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     )
     cur = conn.cursor()
     cur.execute(
-        """SELECT cities.name FROM cities 
-        JOIN states ON cities.state_id = states.id 
+        """SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.id ASC""",
         (argv[4],),
