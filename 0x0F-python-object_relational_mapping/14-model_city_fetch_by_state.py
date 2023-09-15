6131fa3c426f9b6c518f02ea2288148f159de41b
Noransaber <noransaber685@gmail.com>
2023-09-15 23:54:32 +0300

    Style updates

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,9 +9,11 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 if __name__ == "__main__":
-      engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-      Session = sessionmaker(bind=engine)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
 
