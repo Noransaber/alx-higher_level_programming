86bd6b0d134886c3a159e971d8d3b935829b10a0
Noransaber <noransaber685@gmail.com>
2023-09-16 15:36:04 +0300

    Updates

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""reates the State “California” with the City
+“San Francisco” from the database hbtn_0e_100_usa"""
+from model_city import City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    new_state = State(name="California")
+    new_city = Citr(name="San Francisco")
+    new_state.append(new_city)
+    session.add_all([new_state, new_city)]
+    session.commit()
+    session.close()    
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python file that contains the class definition of a State and an instance
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that inherits from Base
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all", "delete")
