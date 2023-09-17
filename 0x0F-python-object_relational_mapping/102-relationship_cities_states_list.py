78012da34186bd7d263283c641a760bce2b6e5ef
Noransaber <noransaber685@gmail.com>
2023-09-17 14:58:28 +0300

    Fixing the style for the final task

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -2,7 +2,7 @@
 """
 Lists all City objects from the database hbtn_0e_101_usa
 """
-from sys import argv
+from sys import av
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
@@ -10,10 +10,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import query
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True,
-    )
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(url.format(av[1], av[2], av[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
