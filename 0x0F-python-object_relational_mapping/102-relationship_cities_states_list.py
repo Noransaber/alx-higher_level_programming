413bddd2de2392725c3924db4c8f7051d20175cf
Noransaber <noransaber685@gmail.com>
2023-09-17 14:45:10 +0300

    Updates

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import query
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]),
         pool_pre_ping=True,
     )
     Base.metadata.create_all(engine)
