3312e7c8ee30f0fe429e3779436f2a2d6a885ea3
Noransaber <noransaber685@gmail.com>
2023-09-17 14:31:05 +0300

    "Updates" This reverts commit 730bc29d6cdd9486f9b16e6907308955df1dfc17.

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -20,4 +20,9 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref=backref"state")
+
+    cities = relationship(
+        "City",
+        cascade="all, delete-orphan",
+        backref=backref("state", cascade="all"),
+        single_parent=True)
