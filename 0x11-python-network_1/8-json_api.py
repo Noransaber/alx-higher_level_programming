c40320279ad5da29721506a5e1137a533d2cc0ce
Noransaber <noransaber685@gmail.com>
2023-09-29 21:02:43 +0300

    Updates

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,15 +7,21 @@ the letter as a parameter.
 
 import requests
 from sys import argv
+import json
 
 if __name__ == "__main__":
     d = {"q": argv[1] if len(argv) > 1 else ""}
-    request = requests.post("http://0.0.0.0:5000/search_user", data=d)
     try:
-        json = request.json()
-        if json:
-            print("[{}] {}".format(json.get("id"), json.get("name")))
-        else:
-            print("No result")
-    except:
-        print("Not a valid JSON")
+        request = requests.post("http://0.0.0.0:5000/search_user", data=d)
+        request.raise_for_status()
+
+        try:
+            jd = request.json()
+            if jd:
+                print("[{}] {}".format(jd.get("id"), jd.get("name")))
+            else:
+                print("No result")
+        except json.JSONDecodeError:
+            print("Invalid JSON response")
+    except requests.RequestException as e:
+        print(f"Request error: {e}")
