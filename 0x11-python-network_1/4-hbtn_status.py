9d2d907e06ae57697d664809f6a8a9328b579263
Noransaber <noransaber685@gmail.com>
2023-09-29 21:41:44 +0300

    Updates

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,11 @@ fetches https://alx-intranet.hbtn.io/status
 
 import requests
 
-if __name__ == "__main__":
-    content = requests.get("https://intranet.hbtn.io/status").text
+
+if __name__ == '__main__':
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
