b03a1c4bf51a221d6a2c14e40793c0cabda2b81e
Noransaber <noransaber685@gmail.com>
2023-09-29 14:12:43 +0300

    Source code

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id
+variable found in the header of the response.
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        print(dict(res.headers).get("X-Request-Id"))
+
