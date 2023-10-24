cae25105b41cf74a6fb97422c036666a919e1971
Noransaber <noransaber685@gmail.com>
2023-10-24 11:39:51 +0300

    Code for writeme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const file = process.argv[2];
+const stringToWrite = process.argv[3];
+
+const fs = require('fs');
+
+fs.writeFile(file, stringToWrite, (error, data) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(data);
+});
